1332066ff916b71eb15c0a5b4a974b2146c521d6
nao-18 <kyahyo17@yahoo.co.jp>
2020-08-22 17:55:30 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,36 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/7.19.0/firebase-app.js"></script>
+//
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/7.19.0/firebase-analytics.js"></script>
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  // apiKey: "AIzaSyClGpqLOFOWR8Kj9S3KKgisDoBJr2Q9HYw",
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
